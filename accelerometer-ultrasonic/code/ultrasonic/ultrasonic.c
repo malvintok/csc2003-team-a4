6dc94def4ffa757ba5f60341412953d0e0d214ff
Walterlayzz <walterlayzz@gmail.com>
2022-11-29 22:59:45 +0800

    Update ultrasonic.c

--- accelerometer-ultrasonic/code/ultrasonic/ultrasonic.c
+++ accelerometer-ultrasonic/code/ultrasonic/ultrasonic.c
@@ -3,21 +3,19 @@
 #include "hardware/gpio.h"
 #include "hardware/timer.h"
 
+#define LeftTrig 10
+#define LeftEcho 11
 #define CenterTrig 14
 #define CenterEcho 15
-#define LeftTrig 0
-#define LeftEcho 0
-#define RightTrig 0
-#define RightEcho 0
+#define RightTrig 16
+#define RightEcho 17
 #define Interval 5000
-#define Cov1 0.000270548 // Sensor without cables
-#define Cov2 0.000147731
 
-const float cov[3] = {0.000270548, 0.000147731, 0};
+const float cov[3] = {0.186389475, 0.000270548, 0.293938391};
 typedef struct KalmanData
 {
     float err_measurement; //Initialise variable
-    float Q; //Noise Covariance Taken from research paper
+    float Q; //Process Noise Covariance (Taken from research paper)
     float KG; //Kalman Gain
     float E; //Estimated State
     float err_estimate; //Error in Estimate
@@ -30,7 +28,6 @@ float val[10];
 
 void init_pins(uint triggerPin, uint echoPin);
 float getDist(int TriggerPin, int EchoPin);
-uint firstReading();
 float sma(float value);
 float des(float value);
 float kalman(float value, int i);
@@ -41,69 +38,51 @@ void init_pins(uint triggerPin, uint echoPin) {
     gpio_init(echoPin);
     gpio_set_dir(triggerPin, GPIO_OUT);
     gpio_set_dir(echoPin, GPIO_IN);
+}
 
+void init_kalmanStruct() {
     for(int i = 0; i < 3; ++i)
     {
         kalmanData[i].err_measurement = cov[i]; //Initialise variable
-        kalmanData[i].Q = 0.001; //Noise Covariance Taken from research paper
+        kalmanData[i].Q = 0.01; //Noise Covariance Taken from research paper
         
         kalmanData[i].KG = 0; //Kalman Gain
         kalmanData[i].E = 0; //Estimated State
-        kalmanData[i].err_estimate = 0.001; //Error in Estimate
+        kalmanData[i].err_estimate = cov[i]; //Error in Estimate
     }
 }
 
 float getDist(int TriggerPin, int EchoPin)
 {
-    static float measurement[5];
-    static float temp = 0;
-    //Take median of 5 readings
-    for (int i=0; i<5; i++)
-    {
-        gpio_put(TriggerPin, true);
-        busy_wait_us(10); // 10 microsecond sleep on trigger high
-        gpio_put(TriggerPin, false); // set trigger pin low
-        gpio_put(EchoPin, false); // set trigger pin low
+    gpio_put(TriggerPin, true);
+    busy_wait_us(10); //  sleep on trigger high
+    gpio_put(TriggerPin, false); // set trigger pin low
+    gpio_put(EchoPin, false); // set trigger pin low
 
-        // wait for echo pin to go high
-        while (gpio_get(EchoPin) == 0) {
-            tight_loop_contents();
-        }
-
-        absolute_time_t startTime = get_absolute_time();
-        while(gpio_get(EchoPin) == 1) {busy_wait_us(1);}//{sleep_us(1);}
-        absolute_time_t endTime = get_absolute_time();
-        uint64_t diffTime = absolute_time_diff_us(startTime, endTime);
-
-        // divide 2 to get the the original distance and multiply by 0.0343 (speed of sound) to get the distance in cm
-        // 0.0343cm/us obtained from velocity of sound which is 343 m/s
-        // 1 / 0.0343 = 29.1
-        float distance = (diffTime / 2) / 29.1;
-
-        measurement[i] = distance;
-        // busy_wait_ms(100);
-        }
-
-        //Sort the array in ascending order    
-    for (int i = 0; i < 5; i++) {     
-        for (int j = i+1; j < 5; j++) {     
-           if(measurement[i] > measurement[j]) {    
-               temp = measurement[i];    
-               measurement[i] = measurement[j];    
-               measurement[j] = temp;    
-           }     
-        }
+    // referred to https://cdn.sparkfun.com/datasheets/Sensors/Proximity/HCSR04.pdf
+    // wait for echo pin to go high (After sending 8 cycle burst, sensor will raise echo value to 1)
+    while (gpio_get(EchoPin) == 0) {
+        tight_loop_contents();
     }
-    int i;
-    if  (TriggerPin == LeftTrig)
-        i = 0;
-    else if  (TriggerPin == CenterTrig)
-        i = 1;
-    else
-        i = 2;
 
+    absolute_time_t startTime = get_absolute_time();
+    // referred to https://lastminuteengineers.com/arduino-sr04-ultrasonic-sensor-tutorial/
+    absolute_time_t timeout = delayed_by_ms(startTime, 38); //Obtain a time after 38ms (Echo Pin goes low after 38ms)
+    while(gpio_get(EchoPin) == 1) {
+        if(time_reached(timeout)) {
+            return 400;
+        }
+        busy_wait_us(1);
+    }
+    absolute_time_t endTime = get_absolute_time();
+    uint64_t diffTime = absolute_time_diff_us(startTime, endTime);
+
+    // divide 2 to get the the original distance and multiply by 0.0343 (speed of sound) to get the distance in cm
+    // 0.0343cm/us obtained from velocity of sound which is 343 m/s
+    // 1 / 0.0343 = 29.1
+    float distance = (diffTime / 2) / 29.1;
         
-    return kalman(measurement[2], i); //Return median
+    return distance; //Return 
 }
 
 
@@ -133,10 +112,9 @@ float sma(float value) {
 }
 
 // Referred to https://www.youtube.com/watch?v=5KEy1OBnkJo&ab_channel=UnfoldDataScience
-// Not sure correct or not
 float des(float value) {
-    static const float a = 0.8; //Weight of alpha (Can change)
-    static const float b = 0.8; //Weight of beta (Can change)
+    static const float a = 0.8; //Weight of alpha
+    static const float b = 0.8; //Weight of beta
     static float pSES = 0; //Previous DES reading
     static float cSES = 0; //Current DES reading
     static float cT = 0; //Trend estimation current
@@ -149,6 +127,8 @@ float des(float value) {
     return (cSES + cT);
 }
 
+// https://www.researchgate.net/publication/330540064_Kalman_Filter_Algorithm_Design_for_HC-SR04_Ultrasonic_Sensor_Data_Acquisition_System
+// Q values referred from paper
 float kalman(float value, int i) { //Value = Measured reading
     //1e-3: Able to reduce some noise and performance is faster
     //1e-5: Able to reduce noise better but have delay
@@ -164,26 +144,6 @@ float kalman(float value, int i) { //Value = Measured reading
     return kalmanData[i].E;
 }
 
-float kalman2(float value, float errM) { //Value = Measured reading
-    //1e-3: Able to reduce some noise and performance is faster
-    //1e-5: Able to reduce noise better but have delay
-    //1e-6: Strong noise reduction but severe delay
-    static float err_measurement = 0; //Initialise variable
-    static const float Q = 0.001; //Noise Covariance Taken from research paper
-    err_measurement = errM; //Measurement Covariance (Obtained from 100 readings)
-    static float KG = 0; //Kalman Gain
-    static float E = 0; //Estimated State
-    static float err_estimate = 0.001; //Error in Estimate
-
-    //Update Kalman Gain
-    KG = err_estimate / (err_estimate + err_measurement);
-    //Estimating current state
-    E = E + (KG*(value - E));
-    //Predict and Update error in estimate
-    err_estimate = ((1-KG)*err_estimate) + Q;
-    //Return estimate value
-    return E;
-}
 
 //Callback function to get distance
 bool repeating_timer_callback(struct repeating_timer *t) {
@@ -197,16 +157,20 @@ int main()
     stdio_init_all();
     sleep_ms(5000);
     printf("Setting up pins...\n");
-    init_pins(CenterTrig, CenterEcho);
+    init_kalmanStruct();
+    init_pins(RightTrig, RightEcho);
     printf("Pins setup complete.\n");
 
-    for (int i = 0; i < 100; i++) {
-        double distance = getDist(CenterTrig, CenterEcho);
-        printf("%f\n", distance);
+    while(true) {
+        float reading = getDist(RightTrig, RightEcho);
+        printf("Raw Measurement: %fcm\n", reading);
+        printf("SMA Measurement: %fcm\n", sma(reading));
+        printf("DES Measurement: %fcm\n", des(reading));
+        printf("KF  Measurement: %fcm\n\n", kalman(reading, 2));
         sleep_ms(500);
     }
 
-    // Timer?
+    // Timer
     // struct repeating_timer timer;
     // add_repeating_timer_ms(Interval, repeating_timer_callback, NULL, &timer);
     // while (1)
